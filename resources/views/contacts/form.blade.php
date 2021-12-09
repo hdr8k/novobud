01ab8fa69bf2d4ebac1870dd7e97cbf8e6faba81
Artem Serbin <49901978+itsserbin@users.noreply.github.com>
2021-12-09 22:42:50 +0200

    Update form.blade.php

--- resources/views/contacts/form.blade.php
+++ resources/views/contacts/form.blade.php
@@ -7,26 +7,26 @@
       })(window,document,'https://cdn.bitrix24.ua/b7708799/crm/form/loader_44.js');
     </script>
     
-    <h4 class="user-form__title">
-        {{__('contacts.questions')}}
-    </h4>
-    <input type="text" name="name" value="" style="position: absolute; left: -9999px; display: none;">
+    // <h4 class="user-form__title">
+    //    {{__('contacts.questions')}}
+    // </h4>
+    // <input type="text" name="name" value="" style="position: absolute; left: -9999px; display: none;">
 
-    <div class="user-form__field">
-        <input class="user-form__input" type="text" name="name_t" placeholder="{{__('contacts.forms.name')}}"
-               required>
-    </div>
+    // <div class="user-form__field">
+    //     <input class="user-form__input" type="text" name="name_t" placeholder="{{__('contacts.forms.name')}}"
+    //            required>
+    // </div>
 
-    <div class="user-form__field">
-        <input class="user-form__input" type="tel" name="phone" placeholder="{{__('contacts.forms.phone')}}"
-               required>
-    </div>
+    // <div class="user-form__field">
+    //     <input class="user-form__input" type="tel" name="phone" placeholder="{{__('contacts.forms.phone')}}"
+    //            required>
+    // </div>
 
-    <div class="user-form__field">
-        <textarea name="text" class="user-form__textarea" placeholder="{{__('contacts.forms.messages')}}"></textarea>
-    </div>
+    // <div class="user-form__field">
+    //     <textarea name="text" class="user-form__textarea" placeholder="{{__('contacts.forms.messages')}}"></textarea>
+    // </div>
 
-    <input type="hidden" name="url" value="{!! url()->current() !!}">
+    // <input type="hidden" name="url" value="{!! url()->current() !!}">
 
-    <button class="uibtn uibtn-primary user-form__submit" type="submit">{{__('contacts.forms.submit')}}</button>
+    // <button class="uibtn uibtn-primary user-form__submit" type="submit">{{__('contacts.forms.submit')}}</button>
 </form>
